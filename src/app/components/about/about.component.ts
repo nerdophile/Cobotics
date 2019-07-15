fb0a6a23e7379eda2651a683c91663cccc01b6c3
venkyyPoojari <venkatpoojari97@gmail.com>
2019-07-16 01:59:18 +0530

    Added About

--- src/app/components/about/about.component.ts
+++ src/app/components/about/about.component.ts
@@ -1,15 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+// tslint:disable:indent
+import {Component, OnInit} from '@angular/core';
 
 @Component({
-  selector: 'app-about',
-  templateUrl: './about.component.html',
-  styleUrls: ['./about.component.css']
+	selector: 'app-about',
+	templateUrl: './about.component.html',
+	styleUrls: ['./about.component.css']
 })
 export class AboutComponent implements OnInit {
+	public paper: any = [];
 
-  constructor() { }
+	constructor() {
+	}
 
-  ngOnInit() {
-  }
+	ngOnInit() {
+		this.paper = [
+			{
+				heading: 'Domain driven approach for coherent rule mining.',
+				details: 'In Advances in Computing,  Communications and Informatics',
+				link: '',
+			},
+			{
+				heading: 'Hyper local listing algorithm for user vicinity based points of interest.',
+				details: 'In Recent Trends in Information Technology (ICRTIT), 2016 International Conference on,pp. 1 - 5. IEEE, 2016.',
+				link: '',
+			}
+		];
+	}
 
 }
