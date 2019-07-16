2bf5ca8f42b965095d23145544276f4827e053a4
venkyyPoojari <venkatpoojari97@gmail.com>
2019-07-16 22:37:38 +0530

    Added Legal

--- src/app/components/about/about.component.ts
+++ src/app/components/about/about.component.ts
@@ -1,5 +1,6 @@
 // tslint:disable:indent
 import {Component, OnInit} from '@angular/core';
+import {log} from 'util';
 
 @Component({
 	selector: 'app-about',
@@ -7,24 +8,47 @@ import {Component, OnInit} from '@angular/core';
 	styleUrls: ['./about.component.css']
 })
 export class AboutComponent implements OnInit {
-	public paper: any = [];
+	private selectedPdf: any;
 
 	constructor() {
 	}
 
+	public paper: any[] = [
+		{
+			id: 0,
+			heading: 'Domain driven approach for coherent rule mining.',
+			details: `In Advances in Computing,  Communications and Informatics
+			(ICACCI), 2013 International Conference on, pp. 109 - 114. IEEE, 2013. `,
+			link: 'http://www.africau.edu/images/default/sample.pdf',
+		},
+		{
+			id: 1,
+			heading: 'Hyper local listing algorithm for user vicinity based points of interest.',
+			details: 'In Recent Trends in Information Technology (ICRTIT), 2016 International Conference on,pp. 1 - 5. IEEE, 2016.',
+			link: 'http://www.africau.edu/images/default/sample.pdf',
+		},
+		{
+			id: 2,
+			heading: 'DUMMY',
+			details: 'In Recent Trends in Information Technology (ICRTIT), 2016 International Conference on,pp. 1 - 5. IEEE, 2016.',
+			link: 'http://www.africau.edu/images/default/sample.pdf',
+		}
+	];
+
 	ngOnInit() {
-		this.paper = [
-			{
-				heading: 'Domain driven approach for coherent rule mining.',
-				details: 'In Advances in Computing,  Communications and Informatics',
-				link: '',
-			},
-			{
-				heading: 'Hyper local listing algorithm for user vicinity based points of interest.',
-				details: 'In Recent Trends in Information Technology (ICRTIT), 2016 International Conference on,pp. 1 - 5. IEEE, 2016.',
-				link: '',
-			}
-		];
+		this.selectedPdf = this.paper[0];
+		console.log(this.selectedPdf.id);
 	}
 
+	incrementPdf() {
+		if (this.selectedPdf.id < this.paper.length - 1) {
+			this.selectedPdf = this.paper [this.selectedPdf.id + 1];
+		}
+	}
+
+	decrementPdf() {
+		if (this.selectedPdf.id > 0) {
+			this.selectedPdf = this.paper [this.selectedPdf.id - 1];
+		}
+	}
 }
