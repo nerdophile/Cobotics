1a5d6402b38d7fa2c0c226f2bfbcc23a15b3242e
venkyyPoojari <venkatpoojari97@gmail.com>
2019-07-19 15:17:57 +0530

    Mobile

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
+// tslint:disable:indent
+
 export const environment = {
-  production: true
+	production: true,
+	firebase: {
+		apiKey: 'AIzaSyBFnd_6OMw4WtZPQW0SrfLX_q4xsrFlv1Q',
+		authDomain: 'https://contactus-fbbcffirebaseio.com.firebaseapp.com',
+		databaseURL: 'https://contactus-fbbcf.firebaseio.com/',
+		projectId: 'https://remics-app.firebaseio.com',
+		storageBucket: 'gs://contactus-fbbcf.appspot.com',
+		messagingSenderId: '315918214898'
+	},
 };
