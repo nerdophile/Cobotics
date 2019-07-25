ccd4682faf8a7bc889006073cdb28f0d40f770c2
venkyyPoojari <venkatpoojari97@gmail.com>
2019-07-25 14:51:47 +0530

    Scrolling

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -23,7 +23,10 @@ const appRoutes: Routes = [
 ];
 
 @NgModule({
-	imports: [RouterModule.forRoot(appRoutes)],
+	imports: [RouterModule.forRoot(appRoutes, {
+			scrollPositionRestoration: 'enabled'
+		}
+	)],
 	exports: [RouterModule]
 })
 
